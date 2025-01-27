5874162ca798b6c49c0e777dd9d296b3b368d010
fallingify <gnswldl000@gmail.com>
2025-01-27 12:25:48 +0900

    reinitialize after remove repo

---
+++ src/main/java/com/g25/mailer/User/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package com.g25.mailer.User.repository;
+
+import com.g25.mailer.User.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByEmail(String email);
+
+    boolean existsByLoginId(String loginId);
+
+    boolean existsById(Long id);
+
+    Optional<User> findByLoginId(String loginId);
+
+}
