28b3c8c8e25ce8d8cd7b6d222f4b4f602d40b75d
fallingify <gnswldl000@gmail.com>
2025-03-30 13:58:18 +0900

    chore: add comment about ai api

--- src/main/java/com/g25/mailer/aiMail/controller/EmailAiController.java
+++ src/main/java/com/g25/mailer/aiMail/controller/EmailAiController.java
@@ -17,12 +17,22 @@ public class EmailAiController {
 
     private final EmailAiService  emailAiService;
 
+    /**
+     * 자동생성
+     * @param request
+     * @return
+     */
     @PostMapping("/generate")
     public ResponseEntity<AutoGenerateResponse> generateEmail(@RequestBody AutoGenerateRequest request) {
         String content = emailAiService.generateEmailContent(request.getPrompt());
         return ResponseEntity.ok(new AutoGenerateResponse(content));
     }
 
+    /**
+     * 문법, 어투 교정
+     * @param request
+     * @return
+     */
     @PostMapping("/refine")
     public ResponseEntity<RefineResponse> refineEmail(@RequestBody RefineRequest request) {
         String refined = emailAiService.refineEmailContent(request.getContent());
