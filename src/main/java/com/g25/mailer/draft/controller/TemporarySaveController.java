b6368b0fe9b37f291b857f512fcbdd7e4a715bfe
fallingify <gnswldl000@gmail.com>
2025-05-01 00:19:46 +0900

    feat : api description added

--- src/main/java/com/g25/mailer/draft/controller/TemporarySaveController.java
+++ src/main/java/com/g25/mailer/draft/controller/TemporarySaveController.java
@@ -3,6 +3,8 @@ package com.g25.mailer.draft.controller;
 import java.util.List;
 
 import com.g25.mailer.draft.service.TemporarySaveService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +17,7 @@ import com.g25.mailer.user.service.UserDetailService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+@Tag(name = "임시저장API", description = "임시메일저장, 전체조회, 전체삭제, 단건삭제 기능")
 @RestController
 @CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/temporary")
@@ -31,6 +34,7 @@ public class TemporarySaveController {
      * @param request
      * @return
      */
+    @Operation(summary = "임시메일저장", description = "작성한 메일을 임시저장합니다.")
     @PostMapping("/write")
     public ResponseEntity<TemporarySaveResponse> createTemporarySave(@RequestBody TemporarySaveRequest request) {
         log.info("임시 저장 content = {}", request.getContent());
@@ -49,6 +53,7 @@ public class TemporarySaveController {
      * 임시 메일함 전체 조회
      * @return
      */
+    @Operation(summary = "임시 메일함 전체 조회", description = "임시메일함 목록을 전체조회합니다.")
     @GetMapping("/list")
     public ResponseEntity<List<TemporarySaveResponse>> listTemporarySaves() {
         List<TemporarySave> saves = temporarySaveService.listTemporarySaves();
@@ -62,6 +67,7 @@ public class TemporarySaveController {
      * 임시 메일함에서 전체 삭제
      * @return
      */
+    @Operation(summary = "임시 메일함 전체 삭제", description = "임시메일함목록에 있는 임시메일을 모두 삭제합니다.")
     @DeleteMapping("/delete-all")
     public ResponseEntity<Void> deleteAllTemp() {
         temporarySaveService.deleteAllTemporarySaves();
@@ -74,6 +80,7 @@ public class TemporarySaveController {
      * @param id
      * @return
      */
+    @Operation(summary = "임시 메일함 단건 삭제", description = "임시메일함목록에 있는 메일 한개를 삭제합니다.")
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteSingleTemp(@PathVariable Long id) {
         temporarySaveService.deleteSingleTemp(id);
