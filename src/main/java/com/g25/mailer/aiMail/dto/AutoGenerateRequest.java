cf3cedf008605330218b455631e264f32666958b
fallingify <gnswldl000@gmail.com>
2025-05-06 09:18:09 +0900

    docs(swagger): AI 메일 요청 DTO Swagger 주석 추가
                                    - AutoGenerateRequest, RefineRequest에 @Schema 설명 및 예제 추가
                                    - Swagger 문서에서 요청 모델 명확화

--- src/main/java/com/g25/mailer/aiMail/dto/AutoGenerateRequest.java
+++ src/main/java/com/g25/mailer/aiMail/dto/AutoGenerateRequest.java
@@ -1,15 +1,19 @@
 package com.g25.mailer.aiMail.dto;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "AI 메일 자동 생성 요청 DTO")
 public class AutoGenerateRequest {
+    @Schema(description = "AI에게 전달할 메일 작성 프롬프트", example = "교수님에게 과제 연장 요청 메일 작성")
     private String prompt;
 }
--- src/main/java/com/g25/mailer/aiMail/dto/RefineRequest.java
+++ src/main/java/com/g25/mailer/aiMail/dto/RefineRequest.java
@@ -1,5 +1,6 @@
 package com.g25.mailer.aiMail.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,8 @@ import lombok.Setter;
 @Getter @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "AI 메일 문법·어투 교정 요청 DTO")
 public class RefineRequest {
+    @Schema(description = "AI가 교정할 메일 원본 내용", example = "교수님 안녕하세요, 과제 제출이 늦어 죄송합니다.")
     private String content;
-}
+}ㅎ
