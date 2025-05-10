250545b26027c9cec538a10f495f497fb1cad73d
fallingify <gnswldl000@gmail.com>
2025-05-10 19:41:38 +0900

    fix: remove files

--- src/main/java/com/g25/mailer/draft/dto/ContentObj.java
+++ src/main/java/com/g25/mailer/draft/dto/ContentObj.java
@@ -4,8 +4,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
-
 @Getter
 @Setter
 @Schema(description = "메일 내용 객체")
@@ -16,6 +14,4 @@ public class ContentObj {
     @Schema(description = "메일 본문 내용", example = "테스트 본문 내용입니다.", required = true)
     private String body;
 
-    @Schema(description = "첨부파일 이름 목록", example = "[\"첨부1.pdf\", \"첨부2.png\"]")
-    private List<String> attachments;
 }
